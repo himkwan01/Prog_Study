38b581bf70168feb93b9813adf17abfe6737cbfc
Tsz Him Kwan <himkwanbbq@hotmail.com>
2016-10-28 21:49:20 -0700

    git script-> repo updated

---
+++ Hw2.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int func(string x, string y){
+    return 0;
+}
+int main(){
+    string x,y;
+    getline(cin,x);
+    getline(cin,y);
+    cou<<func(x,y);
+    return 0;
+}
