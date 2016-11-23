47d92925ab37415b1ffbfeb1849e43b763325343
Tsz Him Kwan <himkwanbbq@hotmail.com>
2016-11-22 21:51:18 -0800

    create php folder and hello world

---
+++ php/hello.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        // tab title
+        <title>PHP Test</title>
+    </head>
+    <body>
+        // php tag
+        <?php echo '<p>Hello World</p>'; ?>
+    </body>
+</html>
